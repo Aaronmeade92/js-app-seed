bf7f3ca02be9846300ae743af7fcbe2aa6f007c6
Aaron <aaronmeade9292@gmail.com>
2019-06-06 18:16:04 -0700

    got the seed up and running locally

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+const sslRedirect = require('heroku-ssl-redirect');
+const express = require('express');
+const PORT = process.env.PORT;
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(sslRedirect());
+app.use(express.static('./public'));
+app.listen(PORT, () => console.log('Listening on PORT', PORT));
